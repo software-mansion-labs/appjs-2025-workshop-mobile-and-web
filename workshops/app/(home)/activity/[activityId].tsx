dc457cab053566ede8b80a0700a266fe5fd9382d
Mateusz Titz <titzmateusz@gmail.com>
2025-05-26 16:20:05 +0200

    1.3

--- workshops/app/(home)/activity/[activityId].tsx
+++ workshops/app/(home)/activity/[activityId].tsx
@@ -2,15 +2,17 @@ import Activity from "@/components/Activity";
 import { useLocalSearchParams } from "expo-router";
 import { ActivityId, getActivityById } from "@/libs/data/dataRepository";
 import NotFoundMessage from "@/components/NotFoundMessage";
+import WithSidePanel from "@/components/WithSidePanel";
 
 export default function ActivityScreen() {
   const { activityId } = useLocalSearchParams<{ activityId: ActivityId }>();
-
   const activity = getActivityById(activityId);
 
-  if (!activity) {
-    return <NotFoundMessage />;
-  }
+  const content = activity ? (
+    <Activity activity={activity} />
+  ) : (
+    <NotFoundMessage />
+  );
 
-  return <Activity activity={activity} />;
+  return <WithSidePanel>{content}</WithSidePanel>;
 }
--- workshops/app/(home)/index.tsx
+++ workshops/app/(home)/index.tsx
@@ -1,7 +1,19 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform, useWindowDimensions } from "react-native";
 import ActivityList from "@/components/ActivityList";
+import { Redirect } from "expo-router";
+import { getActivities } from "@/libs/data/dataRepository";
 
 export default function HomeScreen() {
+  const { width } = useWindowDimensions();
+  const isWideScreen = Platform.OS === "web" && width > 800;
+
+  if (isWideScreen) {
+    const activities = getActivities();
+    const firstActivity = activities[0];
+
+    return <Redirect href={`/activity/${firstActivity.activityId}`} />;
+  }
+
   return (
     <View style={styles.container}>
       <ActivityList />
